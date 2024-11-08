a3daedcb2a1b946bc471df58d92a027081e1686e
Jesse Rusak <rusak.jesse@gmail.com>
2024-11-08 16:12:42 +0000

    feat(ducky/one2sf): TESMART KVM switch inputs on arrow keys

--- MY_BUILD.sh
+++ MY_BUILD.sh
@@ -2,6 +2,7 @@
 
 set -eoux
 
-qmk compile -kb keychron/q8/ansi_encoder -km jerus
+qmk compile -kb ducky/one2sf/1967st -km jerus
+# qmk compile -kb keychron/q8/ansi_encoder -km jerus
 
 rsync -av --delete ./.build/ /mnt/hgfs/Shared_VM_Folder/QMK_BUILD
